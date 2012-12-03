f539b8e049fcbf6a4656e7ce6d9b5b7549b3bcaf
ei11030 <ei11030@B03AV1014>
2012-12-03 16:03:57 +0000

    Adicionado Testes

---
+++ test/testsum2.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ei11030
+ */
+public class testsum2 {
+    
+    public testsum2() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void sum2(){
+        assertEquals(4, 2+2);
+    }
+}
