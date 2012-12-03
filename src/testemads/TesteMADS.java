a7637de03b4389bd859549a153d0aaa50b98fb84
ei11030 <ei11030@B03AV1014>
2012-12-03 15:32:38 +0000

    init

---
+++ src/testemads/TesteMADS.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testemads;
+
+/**
+ *
+ * @author ei11030
+ */
+public class TesteMADS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
